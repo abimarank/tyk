0c9bfb4c34c7b9706114e546a22d54b75c70208f
Yaara Letz <3155222+letzya@users.noreply.github.com>
2019-02-27 20:23:15 +0000

    Add option to meta data on response wip

--- res_handler_transform.go
+++ res_handler_transform.go
@@ -98,6 +98,7 @@ func (h *ResponseTransformMiddleware) HandleResponse(rw http.ResponseWriter, res
 		bodyData, err = mxj.NewMapXml(body) // unmarshal
 		if err != nil {
 			logger.WithError(err).Error("Error unmarshalling XML")
+			//todo return error
 		}
 	default: // apidef.RequestJSON
 		if len(body) == 0 {
@@ -107,6 +108,7 @@ func (h *ResponseTransformMiddleware) HandleResponse(rw http.ResponseWriter, res
 		var tempBody interface{}
 		if err := json.Unmarshal(body, &tempBody); err != nil {
 			logger.WithError(err).Error("Error unmarshalling JSON")
+			//todo return error
 		}
 
 		switch tempBody.(type) {
@@ -122,8 +124,12 @@ func (h *ResponseTransformMiddleware) HandleResponse(rw http.ResponseWriter, res
 	}
 
 	if tmeta.TemplateData.EnableSession {
-		session := ctxGetSession(req)
-		bodyData["_tyk_meta"] = session.MetaData
+		if session := ctxGetSession(req); session != nil {
+			bodyData["_tyk_meta"] = session.MetaData
+		} else {
+			logger.Error("Session context was not found.")
+			//todo return error
+		}
 	}
 
 	// Apply to template
